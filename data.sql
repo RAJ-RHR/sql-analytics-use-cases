362321ea1b78a631e0a654c703055c2b77a23e38
Raj Hansh Raj <rajhansraj2000@gmail.com>
2025-12-29 17:48:53 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO sales VALUES
+(1001,'2024-01-02',501,'Laptop','Electronics',1,55000,'North'),
+(1002,'2024-01-02',502,'Mouse','Electronics',2,500,'North'),
+(1003,'2024-01-03',503,'Keyboard','Electronics',1,1200,'East'),
+(1004,'2024-01-03',504,'Office Chair','Furniture',1,7000,'West');
