55d88a0dde759818b9efae41becfd3af5189654d
Raj Hansh Raj <rajhansraj2000@gmail.com>
2025-12-29 17:48:33 +0530

    Create sales table in schema.sql

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales (
+    order_id INT,
+    order_date DATE,
+    customer_id INT,
+    product VARCHAR(100),
+    category VARCHAR(50),
+    quantity INT,
+    unit_price DECIMAL(10,2),
+    region VARCHAR(50)
+);
